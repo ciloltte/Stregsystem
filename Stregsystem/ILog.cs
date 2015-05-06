21b9ec45412d4ac647930227e86baae8f5b9f9d9
siraggi <august@aggisoft.dk>
2015-05-06 10:47:39 +0200

    Added classes and made ILog

---
+++ Stregsystem/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stregsystem
+{
+    interface ILog
+    {
+        void SaveTransaction(Transaction transaction);
+        string[] ReadTransactions(int numTransactionsToRaed);
+        string[] ReadTransactionsByUserId(int userId);
+    }
+}
---
+++ Stregsystem/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stregsystem
+{
+    class Log : ILog
+    {
+        public void SaveTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] ReadTransactions(int numTransactionsToRaed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] ReadTransactionsByUserId(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
